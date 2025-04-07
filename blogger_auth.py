66377fa9c72a5bd05b948645d2f7de2eb261dc13
corak123 <advanadesain@gmail.com>
2025-04-07 22:04:41 +0700

    Update blogger_auth.py

--- blogger_auth.py
+++ blogger_auth.py
@@ -71,7 +71,8 @@ def get_authenticated_service():
     
     scopes = ["https://www.googleapis.com/auth/blogger"]
     flow = InstalledAppFlow.from_client_config(client_config, scopes=scopes)
-    credentials = flow.run_local_server(port=0)
+    credentials = flow.run_console()
+
 
 
     return credentials
