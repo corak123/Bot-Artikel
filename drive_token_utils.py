6b1f692bba9280af93a77a1aef63bc0af1932c3d
corak123 <advanadesain@gmail.com>
2025-04-08 21:41:25 +0700

    Update drive_token_utils.py

--- drive_token_utils.py
+++ drive_token_utils.py
@@ -1,67 +1,88 @@
-import io
 import pickle
+import io
 import os
-import streamlit as st
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
 from google.oauth2.credentials import Credentials
+import streamlit as st
 
-# Ambil drive_folder_id dari st.secrets
-DRIVE_FOLDER_ID = st.secrets["google_drive"]["folder_id"]
+# Folder Google Drive untuk menyimpan token
+DRIVE_FOLDER_NAME = "blogger_tokens"
 
-# Inisialisasi Google Drive API pakai kredensial app
 def get_drive_service():
-    creds = Credentials.from_authorized_user_info(info=st.secrets["google_oauth"], scopes=["https://www.googleapis.com/auth/drive"])
-    return build('drive', 'v3', credentials=creds)
+    drive_creds = Credentials.from_authorized_user_info(st.secrets["drive_oauth"])
+    service = build("drive", "v3", credentials=drive_creds)
+    return service
 
-# Simpan token user ke Google Drive
-def upload_token_to_drive(creds, email):
-    service = get_drive_service()
+def find_or_create_folder(service):
+    # Cek apakah folder sudah ada
+    query = f"name='{DRIVE_FOLDER_NAME}' and mimeType='application/vnd.google-apps.folder' and trashed = false"
+    results = service.files().list(q=query, fields="files(id, name)").execute()
+    items = results.get("files", [])
+
+    if items:
+        return items[0]["id"]  # return folder ID
+    else:
+        # Jika belum ada, buat folder baru
+        file_metadata = {
+            "name": DRIVE_FOLDER_NAME,
+            "mimeType": "application/vnd.google-apps.folder"
+        }
+        folder = service.files().create(body=file_metadata, fields="id").execute()
+        return folder["id"]
+
+def upload_token_to_drive(email, creds):
     safe_email = email.replace("@", "_").replace(".", "_")
     filename = f"token_{safe_email}.pkl"
 
-    # Serialize token
-    token_bytes = pickle.dumps(creds)
-    media = MediaIoBaseUpload(io.BytesIO(token_bytes), mimetype='application/octet-stream')
+    # Serialize credentials
+    creds_bytes = pickle.dumps(creds)
+    fh = io.BytesIO(creds_bytes)
+
+    service = get_drive_service()
+    folder_id = find_or_create_folder(service)
 
     # Cek apakah file sudah ada
-    query = f"name='{filename}' and '{DRIVE_FOLDER_ID}' in parents"
-    results = service.files().list(q=query, spaces='drive', fields='files(id)').execute()
-    files = results.get('files', [])
+    query = f"name='{filename}' and '{folder_id}' in parents and trashed = false"
+    result = service.files().list(q=query, fields="files(id)").execute()
+    items = result.get("files", [])
 
-    if files:
+    media = MediaIoBaseUpload(fh, mimetype="application/octet-stream")
+
+    if items:
         # Replace existing file
-        file_id = files[0]['id']
+        file_id = items[0]["id"]
         service.files().update(fileId=file_id, media_body=media).execute()
     else:
-        # Upload baru
+        # Upload new file
         file_metadata = {
-            'name': filename,
-            'parents': [DRIVE_FOLDER_ID]
+            "name": filename,
+            "parents": [folder_id]
         }
-        service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        service.files().create(body=file_metadata, media_body=media).execute()
 
-# Ambil token user dari Google Drive
-def download_token_from_drive(email):
-    service = get_drive_service()
+def load_token_from_drive(email):
     safe_email = email.replace("@", "_").replace(".", "_")
     filename = f"token_{safe_email}.pkl"
 
-    query = f"name='{filename}' and '{DRIVE_FOLDER_ID}' in parents"
-    results = service.files().list(q=query, spaces='drive', fields='files(id)').execute()
-    files = results.get('files', [])
+    service = get_drive_service()
+    folder_id = find_or_create_folder(service)
 
-    if not files:
+    query = f"name='{filename}' and '{folder_id}' in parents and trashed = false"
+    result = service.files().list(q=query, fields="files(id)").execute()
+    items = result.get("files", [])
+
+    if not items:
         return None
 
-    file_id = files[0]['id']
-    request = service.files().get_media(fileId=file_id)
+    file_id = items[0]["id"]
     fh = io.BytesIO()
+    request = service.files().get_media(fileId=file_id)
     downloader = MediaIoBaseDownload(fh, request)
-
     done = False
-    while done is False:
-        status, done = downloader.next_chunk()
+    while not done:
+        _, done = downloader.next_chunk()
 
     fh.seek(0)
-    return pickle.load(fh)
+    creds = pickle.load(fh)
+    return creds
