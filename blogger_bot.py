2ba3f4b007d5633818c8258f9263eb420afb2a26
corak123 <advanadesain@gmail.com>
2025-04-08 10:39:51 +0700

    Update blogger_bot.py

--- blogger_bot.py
+++ blogger_bot.py
@@ -115,7 +115,7 @@ def show_login_prompt():
     st.write("🔐 Klik tombol di bawah untuk login dengan Google")
 
     if st.button("Login dengan Google"):
-        #credentials = get_authenticated_service()
+        credentials = get_authenticated_service()
 
         if credentials:
             st.write("🔐 berhasil kredensial")
@@ -138,7 +138,7 @@ def show_login_prompt():
 
 # ✅ Cek apakah user baru logout
 if st.session_state.get("just_logged_out"):
-    show_login_prompt()
+    #show_login_prompt()
     st.stop()
 
 # ✅ Jika belum login
