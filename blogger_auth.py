b0ca51f52a95bab611a4630d6bd96344d74922a8
corak123 <advanadesain@gmail.com>
2025-04-12 17:59:39 +0700

    Update blogger_auth.py

--- blogger_auth.py
+++ blogger_auth.py
@@ -76,5 +76,6 @@ def get_authenticated_service():
             access_type='offline',
             include_granted_scopes='true'
         )
+        
         st.markdown(f"[🔐 Klik di sini untuk login Google]({auth_url})", unsafe_allow_html=True)
         st.stop()
