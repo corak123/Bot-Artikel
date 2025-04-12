fd092e0078266b7b2bbdaab04eabd6445a340fb1
corak123 <advanadesain@gmail.com>
2025-04-12 18:25:01 +0700

    Update blogger_auth.py

--- blogger_auth.py
+++ blogger_auth.py
@@ -76,6 +76,7 @@ def get_authenticated_service():
             access_type='offline',
             include_granted_scopes='true'
         )
-        if st.button(f"🔐 Login Google {auth_url}", unsafe_allow_html=True):
-            st.markdown(f"[🔐 Klik di sini untuk login Google]({auth_url})", unsafe_allow_html=True)
+        if st.button("🔐 Login Google"):
+            st.markdown(f'<meta http-equiv="refresh" content="0;url={auth_url}">', unsafe_allow_html=True)
             st.stop()
+
