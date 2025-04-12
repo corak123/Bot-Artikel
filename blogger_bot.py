c87e4f78359bce0eb5fd45d8463cea84c078262f
corak123 <advanadesain@gmail.com>
2025-04-12 17:56:53 +0700

    Update blogger_bot.py

--- blogger_bot.py
+++ blogger_bot.py
@@ -4,36 +4,11 @@ from blogger_auth import get_authenticated_service
 st.set_page_config(page_title="Bot Artikel Blogger Otomatis", page_icon="🤖")
 st.title("🤖 Bot Artikel Blogger Otomatis")
 
-def login():
-    # Simulasi proses login dengan tombol
-    if st.button("Login"):
-        # Cek apakah sudah ada auth code dari URL
-        flow = Flow.from_client_config(
-            client_config,
-            scopes=SCOPES,
-            redirect_uri="https://bot-artikel-auto.streamlit.app/"
-        )
-        query_params = st.query_params
-        code = query_params.get("code")
-        st.write("Isi kode:", code)
-        auth_url, _ = flow.authorization_url(
-            prompt='consent',
-            access_type='offline',
-            include_granted_scopes='true'
-        )
-        st.markdown(f"[🔐 Klik di sini untuk login Google]({auth_url})", unsafe_allow_html=True)
-        # Ini misalnya kode yang didapat setelah login
-        st.session_state.code = code  # Simpan code di session_state agar tetap ada selama sesi
-        st.success("Login berhasil!")
-        return code
-    return None
-
 # Fungsi Logout
 def logout():
     for key in ["credentials", "user_email", "user_name", "user_picture", "code"]:
         if key in st.session_state:
             del st.session_state[key]
-            st.success("✅ Kamu berhasil logout.")
             st.markdown(
                 '<meta http-equiv="refresh" content="0;url=https://bot-artikel-auto.streamlit.app/">',
                 unsafe_allow_html=True
