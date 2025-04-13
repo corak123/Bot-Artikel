b07a7549501d484e981650e39efff21a4ff84d25
corak123 <advanadesain@gmail.com>
2025-04-13 14:37:19 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -51,6 +51,7 @@ st.markdown("""
         border-radius: 12px;
         box-shadow: 0 4px 6px rgba(0,0,0,0.1);
         text-align: center;
+        width: 100px !important;
     }
     .sidebar-profil img {
         border-radius: 50%;
